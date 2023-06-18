6d32135d91f2c056d1a571731a591ca314b9e926
Aleena <aleenaalam2006@gmail.com>
2023-06-17 23:35:42 -0700

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-def main():
-
+def main(): 
+    
     ##############################
     # make your code below
     # print('Hello World')
     ##############################
-
+    print ('Hello World')
+    print ('CS7: Python Programming')
+   
 
 if __name__ == '__main__':
     main()
